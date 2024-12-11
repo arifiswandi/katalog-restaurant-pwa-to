32524a42b56cf0838927e6ec52074650d6d397e0
Arif Iswandi <arifiswandi81@gmail.com>
2024-12-11 09:37:01 +0700

    Add github action workflow: ci.yaml

--- tests/favoriteRestaurantIdb.test.js
+++ tests/favoriteRestaurantIdb.test.js
@@ -2,7 +2,7 @@ import { describe, afterEach } from '@jest/globals';
 import { itActsAsPreferredRestaurantModel } from './contracts/favoriteContract';
 import preferredRestaurantIdb from '../src/scripts/data/favorite-idb';
 
-describe('Favorite Movie Idb Contract Test Implementation', () => {
+describe('Favorite Restaurant Idb Contract Test Implementation', () => {
   afterEach(async () => {
     (await preferredRestaurantIdb.getAllRestaurants()).forEach(async (restaurant) => {
       await preferredRestaurantIdb.deleteRestaurant(restaurant.id);
